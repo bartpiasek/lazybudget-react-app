48340fb10d0335790c7440aeb5b93128211d368c
Bart Piasek <bartpiasek@icloud.com>
2021-04-03 23:06:35 +0200

    expenses/incomes component - database

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import Cashflow from "../Main/Cashflow/Cashflow";
 import TransactionRow from "./TrUI";
 
 const History = () => {
@@ -29,9 +28,15 @@ const History = () => {
     .reverse()
     .map((transaction) => transaction.amount);
 
-  const total = transactionsAmount.reduce(function (a, b) {
-    return a + b;
-  });
+  const total = transactionsAmount.reduce((a, b) => a + b, 0);
+
+  const expenses = transactionsAmount
+    .filter((amount) => amount < 0)
+    .reduce((a, b) => a + b, 0);
+
+  const incomes = transactionsAmount
+    .filter((amount) => amount > 0)
+    .reduce((a, b) => a + b, 0);
 
   //FILTER TRANSACTION OBJ - EXPENSES/INCOMES - sorting
   const firebaseExpenses = transactionsObj.filter((transaction) => {
@@ -45,20 +50,19 @@ const History = () => {
   return (
     <div>
       <div>
-        <Cashflow />
         <h5>Total balance</h5>
         <h2>{total.toFixed(2)} PLN</h2>
         <h5>Expenses</h5>
-        <h2> PLN</h2>
+        <h2>{Math.abs(expenses).toFixed(2)} PLN</h2>
         <h5>Incomes</h5>
-        <h2> PLN</h2>
+        <h2>{incomes.toFixed(2)} PLN</h2>
       </div>
       <hr />
       <div>
         <div>
           <h5>Last transactions</h5>
           <div>
-            {transactionsObj.reverse().map((transaction, key) => {
+            {transactionsObj.map((transaction, key) => {
               return (
                 <div key={key}>
                   <TransactionRow
