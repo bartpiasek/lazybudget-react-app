a7487456b8192641ec3767e166bbfb00f01117d1
Bart Piasek <bartpiasek@icloud.com>
2021-07-19 22:57:05 +0200

    rwd table

--- src/components/History/TransactionUI.js
+++ src/components/History/TransactionUI.js
@@ -8,20 +8,18 @@ const TransactionRow = ({ text, amount, option, date, category, id }) => {
         <div className="text">
           <h5>{text}</h5>
         </div>
-        <div className="data">
-          <div>
-            <p className="category">{category}</p>
-          </div>
-          <div>
-            <p>{date.split("T")[0]}</p>
-          </div>
-          {option === "expense" ? (
-            <p className="price red">{option}</p>
-          ) : (
-            <p className="price green">{option}</p>
-          )}
-          <p className="price">{amount.toFixed(2)} PLN</p>
+        <div>
+          <p className="category">{category}</p>
         </div>
+        <div className="date">
+          <p>{date.split("T")[0]}</p>
+        </div>
+        {option === "expense" ? (
+          <p className="price red">{option}</p>
+        ) : (
+          <p className="price green">{option}</p>
+        )}
+        <p className="price">{amount.toFixed(2)} PLN</p>
       </div>
     </div>
   );
