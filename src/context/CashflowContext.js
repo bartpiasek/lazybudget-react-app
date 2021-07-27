e9f65f2c82c83dc9e590d584d58d26250388e1db
Bart Piasek <bartpiasek@icloud.com>
2021-07-27 22:47:20 +0200

    DONE: local storage for settings (Dashboard goals)

--- src/context/CashflowContext.js
+++ src/context/CashflowContext.js
@@ -9,13 +9,20 @@ export const CashflowProvider = (props) => {
 
   useEffect(() => {
     const localIncomeGoal = localStorage.getItem("local-incomeGoal");
-    if (localIncomeGoal) {
+    const localExpenseGoal = localStorage.getItem("local-expenseGoal");
+    const localSavingGoal = localStorage.getItem("local-savingGoal");
+
+    if (localIncomeGoal && localExpenseGoal && localSavingGoal) {
       setIncomeGoal(JSON.parse(localIncomeGoal));
+      setExpenseGoal(JSON.parse(localExpenseGoal));
+      setSavingGoal(JSON.parse(localSavingGoal));
     }
   }, []);
 
   useEffect(() => {
     localStorage.setItem("local-incomeGoal", JSON.stringify(incomeGoal));
+    localStorage.setItem("local-expenseGoal", JSON.stringify(expenseGoal));
+    localStorage.setItem("local-savingGoal", JSON.stringify(savingGoal));
   });
 
   const value = {
